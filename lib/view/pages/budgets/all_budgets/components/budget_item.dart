9c3a6e15bf7320536faab7f436836dd524ad35af
Ahmed Arafat <ahmed.arafat3535@gmail.com>
2023-09-13 16:40:59 +0300

    fix: refactor the structure of budget view

---
+++ lib/view/pages/budgets/all_budgets/components/budget_item.dart
@@ -0,0 +1,45 @@
+part of '../budgets_page.dart';
+
+class BudgetItem extends StatelessWidget {
+  final String name;
+  final double amount;
+  const BudgetItem({
+    super.key,
+    required this.name,
+    required this.amount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15.w),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            children: [
+              PublicText(
+                txt: name,
+              ),
+              const Spacer(),
+              PublicText(
+                txt: "${S.of(context).egp} ${amount.orAbout()}",
+              ),
+              10.pw,
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.edit_outlined,
+                  color: AppColors.mintGreen,
+                  size: 20.w,
+                ),
+              ),
+            ],
+          ),
+          PublicDivider(width: 230.w),
+        ],
+      ),
+    );
+  }
+}
