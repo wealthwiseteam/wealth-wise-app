75f73c6a1e8eb9ce4e6c2789a7918388414d1ead
Ahmed Arafat <ahmed.arafat3535@gmail.com>
2023-09-15 21:14:39 +0300

    feat: add bloc observer

--- lib/resources/service_locator/service_locator.dart
+++ lib/resources/service_locator/service_locator.dart
@@ -1,8 +1,10 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wealth_wise/data/source/local/app_prefs.dart';
 import 'package:wealth_wise/data/source/remote/api_service.dart';
+import 'package:wealth_wise/view_model/bloc_observer.dart';
 
 import '../../data/network/network_info.dart';
 import '../../data/repository/auth_repository.dart';
@@ -10,6 +12,8 @@ import '../../data/repository/auth_repository.dart';
 final GetIt getIt = GetIt.instance;
 
 Future<void> initModule() async {
+  Bloc.observer = MyBlocObserver();
+
   /// Shared prefs
   final sharedPrefs = await SharedPreferences.getInstance();
   getIt.registerLazySingleton(
